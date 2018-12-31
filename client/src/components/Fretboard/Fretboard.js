bd3c45195d158adb88057822620fa1eeb0545a28
Derek Schubert <derek@starbound.digital>
2018-12-31 14:01:45 -0800

    pre-dev checkout

--- client/src/components/Fretboard/Fretboard.js
+++ client/src/components/Fretboard/Fretboard.js
@@ -10,7 +10,6 @@ import ChordFinder from '../FindChord/FindChord';
 export default () => {
   const { controls } = useContext(ControlsContext);
   let notesInScale = findNotesInSelectedScale({ controls });
-  let [chordFinder, setChordFinder] = useState(false);
 
   let tuning = [...controls.tuning].reverse();
 
@@ -21,8 +20,6 @@ export default () => {
       {tuning.map((t, i) => <String key={i} notesInScale={notesInScale} openNote={t} controls={controls} capo={controls.capo} />)}
 
       {controls.showBottomFretPositions && <FretPositions capo={controls.capo} frets={controls.frets} />}
-      {chordFinder && <ChordFinder />}
-      <button onClick={() => setChordFinder(!chordFinder)}>{chordFinder ? 'Hide Chord Finder' : 'Find Chords'}</button>
     </div>
   );
 }
